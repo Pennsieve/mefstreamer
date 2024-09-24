534cb50a4b876254a74bd27c5c3282f8ee546783
Julia Dengler <73361030+j-dengler@users.noreply.github.com>
2024-09-24 14:31:49 -0400

    adding correct header for csv writing to main

--- src/main/java/edu/upenn/cis/eeg/mef/mefstreamer/MEFStreamerMain.java
+++ src/main/java/edu/upenn/cis/eeg/mef/mefstreamer/MEFStreamerMain.java
@@ -1,6 +1,7 @@
 package edu.upenn.cis.eeg.mef.mefstreamer;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.RandomAccessFile;
@@ -17,17 +18,25 @@ public class MEFStreamerMain {
             System.out.println("Usage: java -jar <jarfile> <filePath>");
             return;
         }
-
+        
         String filePath = args[0];
+        File inputFile = new File(filePath);
+        String baseName = inputFile.getName().substring(0, inputFile.getName().lastIndexOf('.'));
+        String directory = inputFile.getParent();  // Get the directory of the input file
+
+        // Construct output file names including the directory
+        String headerFileName = directory + File.separator + baseName + "_header_info.csv";
+        String valuesFileName = directory + File.separator + baseName + "_values_data.csv";
+
+
         try (RandomAccessFile file = new RandomAccessFile(filePath, "r")) {
             MEFStreamer streamer = new MEFStreamer(file);
             MefHeader2 header = streamer.getMEFHeader();
 
-         // Write header information to CSV
-            try (BufferedWriter headerWriter = new BufferedWriter(new FileWriter("/Users/juliadengler/Documents/mefstreamer/header_info.csv"))) {
-                headerWriter.write("Channel Name,GMT OFFSET,Sampling Frequency,Recording Start Time,Recording End Time,Min Value,Max Value\n");
-                headerWriter.write(String.format("%s,%s,%s,%s,%s,%s,%s\n",
-                        header.get
+         // Write header information to CSV 53
+            try (BufferedWriter headerWriter = new BufferedWriter(new FileWriter(headerFileName))) {
+                headerWriter.write("Institution,Unencrypted Text Field,Encryption Algorithm,Subject Encryption Used,Session Encryption Used,Data Encryption Used,Byte Order Code,Header Version Major,Header Version Minor,Header Length,Session Unique ID,Subject First Name,Subject Middle Name,Subject Last Name,Subject ID,Session Password,Number of Index Entries,Channel Name,Recording State Time,Recording End Time,Sampling Frequency,Low Frequency Filter Setting,High Frequency Filter Setting,Notch Filter Frequency,Voltage Conversion Factor,Acquisition System,Channel Comments,Study Comments,Physical Channel Number,Compression Algorithm,Maximum Compressed Block Size,Maximum Block Length,Block Interval,Maximum Data Value,Minimum Data Value,Block Header Length,GMT Offset,Discontinuity Data Offset,Number of Discontinuity Entries,File Unique ID,Anonymized Subject Name,Header CRC,8 Random Bytes,Bytes Read,Class,File Name,Index Data Offset,Index Data Offset End,Session Validation Field,Subject Validation Field,Voltage Offset,CRC Tab 32\n");
+                headerWriter.write(String.format("%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n",
                 		header.getInstitution(), // string
                         header.getUnencryptedTextField(), //string
                         header.getEncryptionAlgorithm(), //string
@@ -44,9 +53,8 @@ public class MEFStreamerMain {
                         header.getSubjectThirdName(), //string
                         header.getSubjectId(), //string
                         header.getSessionPassword(), //string
-                        // missing subject passowrd validation field
+                        // missing subject password validation field
                         // missing protected region
-                        header.getSessionPassword(), //string
                         String.valueOf(header.getNumberOfIndexEntries()), //long
                 		header.getChannelName(), //string
                         header.getRecordingStartTime(), //string
@@ -76,7 +84,18 @@ public class MEFStreamerMain {
                         // missing unused
                         String.valueOf(header.getFileUniqueID()), //byte
                         header.getAnonymizedSubjectName(), //string
-                        String.valueOf(header.getHeaderCrc()))); //byte
+                        String.valueOf(header.getHeaderCrc()),
+                        String.valueOf(header.get8RandomBytes()),
+                        String.valueOf(header.getBytesRead()),
+                        String.valueOf(header.getClass()),
+                        header.getFilename(),
+                        String.valueOf(header.getIndexDataOffset()),
+                        String.valueOf(header.getIndexDataOffsetEnd()),
+                        String.valueOf(header.getNumberOfSamples()),
+                        header.getSessionValidationField(),
+                        header.getSubjectValidationField(),
+                        String.valueOf(header.getVoltageOffset()),
+                        String.valueOf(header.getCrcTab32()))); //byte
                         // missing EEG Data Start
             }
 
@@ -86,7 +105,7 @@ public class MEFStreamerMain {
             System.out.println("Num Blocks: " + numBlocks + " blocks.");
 
             // Write values to CSV
-            try (BufferedWriter valuesWriter = new BufferedWriter(new FileWriter("/Users/juliadengler/Documents/mefstreamer/values_data.csv"))) {
+            try (BufferedWriter valuesWriter = new BufferedWriter(new FileWriter(valuesFileName))) {
                 valuesWriter.write("Time Start,Time End,Value\n");
 
                 // Loop over each page within the blocks
